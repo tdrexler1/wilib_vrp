b6ba8c1073f5e735e560bef99389a1c786ba92cc
tdrexler1 <42881144+tdrexler1@users.noreply.github.com>
2021-08-29 16:28:16 -0500

    starter region 1 results

--- wilib_vrp_modelrun.bat
+++ wilib_vrp_modelrun.bat
@@ -2,7 +2,7 @@ SETLOCAL EnableDelayedExpansion
 FOR %%G IN (40, 60, 200, 0) DO (
     FOR %%H IN (8, 10) DO (
         FOR %%I IN (savings, christofides, parallel_cheapest_insertion, automatic) DO (
-            python C:\Users\tdrex\PycharmProjects\wilib_vrp\wi_lib_vrp.py wi_library_directory_geo_reg.csv ideal 7 duration 1 %%H 500 %%G 30 %%I automatic --vehicle_increment --text_file --map
+            python C:\Users\tdrex\PycharmProjects\wilib_vrp\wi_lib_vrp.py wi_library_directory_geo_reg.csv starter 1 duration 1 %%H 500 %%G 30 %%I automatic --vehicle_increment --text_file --map
         )
     )
 )
